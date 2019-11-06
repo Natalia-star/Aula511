fe33f946fe3e61ec6d76d57816e102d4d1c8f15c
lyvisson <lyvissonmarinho@gmail.com>
2019-11-05 21:44:20 -0300

    Com comentario

---
+++ main.py
@@ -0,0 +1,11 @@
+a = 1
+b = -5
+
+soma = a + b
+
+if(soma > a): #Soma maior que a
+    print("soma maior que a")
+elif(soma == a): #soma igual
+    print("soma igual a a")
+else: #Se é menor que a
+    print(" a soma menor que a")#resultado
